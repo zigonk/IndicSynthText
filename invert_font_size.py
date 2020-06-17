b02bf1822995ded82fac78468b9b9054571b231b
azhar <azhar199865@gmail.com>
2020-06-17 16:39:32 +0530

    modified script

--- invert_font_size.py
+++ invert_font_size.py
@@ -7,32 +7,48 @@ from pygame import freetype
 from text_utils import FontState
 import numpy as np 
 import matplotlib.pyplot as plt 
-import cPickle as cp
+import _pickle as cPickle
+import argparse
 
 
-pygame.init()
+def gen_fontmodel(lang,output_path):
+	pygame.init()
 
 
-ys = np.arange(8,200)
-A = np.c_[ys,np.ones_like(ys)]
+	ys = np.arange(8,200)
+	A = np.c_[ys,np.ones_like(ys)]
 
-xs = []
-models = {} #linear model
+	xs = []
+	models = {} #linear model
 
-FS = FontState()
-#plt.figure()
-for i in xrange(len(FS.fonts)):
-	print i
-	font = freetype.Font(FS.fonts[i], size=12)
-	h = []
-	for y in ys:
-		h.append(font.get_sized_glyph_height(y))
-	h = np.array(h)
-	m,_,_,_ = np.linalg.lstsq(A,h)
-	models[font.name] = m
-	xs.append(h)
+	FS = FontState(lang,create_model=True)
+	print(FS)
+	#plt.figure()
+	#plt.hold(True)
+	for i in range(len(FS.fonts)):
+		print(i)
+		font = freetype.Font(FS.fonts[i], size=12)
+		h = []
+		for y in ys:
+			y = float(y)
+			h.append(font.get_sized_glyph_height(y))
+		h = np.array(h)
+		m,_,_,_ = np.linalg.lstsq(A,h)
+		print(m)
+		print(font.name)
+		models[font.name] = m
+		xs.append(h)
 
-with open('font_px2pt.cp','w') as f:
-	cp.dump(models,f)
-#plt.plot(xs,ys[i])
-#plt.show()
+	with open(output_path+'/font_px2pt'+lang+'.cp','wb') as f:
+		cPickle.dump(models,f)
+	#plt.plot(xs,ys[i])
+	#plt.show()
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--lang',help='flag for turning on visualizations')
+	parser.add_argument('--output_path',help='path to store font models')
+	args = parser.parse_args()
+	gen_fontmodel(args.lang,args.output_path)
+
+
